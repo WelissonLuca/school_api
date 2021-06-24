83bdcb879b77227b159c16d29fbb72ea0dd0010d
WelissonLuca <welissonluca17@gmail.com>
2021-06-24 00:11:01 -0300

    feat: register singleton user repository inject

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,1 +1,6 @@
 import { container } from 'tsyringe';
+
+import { UserRepository } from '@modules/school/infra/typeorm/repositories/UserRepository';
+import { IUserRepository } from '@modules/school/repositories/IUserRepository';
+
+container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
