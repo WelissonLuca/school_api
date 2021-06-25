86390904f41423a32e95f88631b25c93aa1ec258
WelissonLuca <welissonluca17@gmail.com>
2021-06-25 11:12:25 -0300

    feat: register singleton question inject

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,9 @@
 import { container } from 'tsyringe';
 
+import { QuestionRepository } from '@modules/school/infra/typeorm/repositories/QuestionRepository';
 import { SchoolTestRepository } from '@modules/school/infra/typeorm/repositories/SchoolTestRepository';
 import { UserRepository } from '@modules/school/infra/typeorm/repositories/UserRepository';
+import { IQuestionRepository } from '@modules/school/repositories/IQuestionRepository';
 import { ISchoolTestRepository } from '@modules/school/repositories/ISchoolTestRepository';
 import { IUserRepository } from '@modules/school/repositories/IUserRepository';
 
@@ -9,4 +11,8 @@ container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
 container.registerSingleton<ISchoolTestRepository>(
   'SchoolTestRepository',
   SchoolTestRepository
+);
+container.registerSingleton<IQuestionRepository>(
+  'QuestionRepository',
+  QuestionRepository
 );
