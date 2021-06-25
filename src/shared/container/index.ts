28fd161cc1573e3bf4c3fcb3799e2c7a2b17a45a
WelissonLuca <welissonluca17@gmail.com>
2021-06-25 10:50:36 -0300

    feat: register singleton school repository inject

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,12 @@
 import { container } from 'tsyringe';
 
+import { SchoolTestRepository } from '@modules/school/infra/typeorm/repositories/SchoolTestRepository';
 import { UserRepository } from '@modules/school/infra/typeorm/repositories/UserRepository';
+import { ISchoolTestRepository } from '@modules/school/repositories/ISchoolTestRepository';
 import { IUserRepository } from '@modules/school/repositories/IUserRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+container.registerSingleton<ISchoolTestRepository>(
+  'SchoolTestRepository',
+  SchoolTestRepository
+);
