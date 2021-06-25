30934f5007475e64c9881b112b832fd77954296b
WelissonLuca <welissonluca17@gmail.com>
2021-06-25 14:37:24 -0300

    feat: add list user route

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -2,13 +2,24 @@ import { Router } from 'express';
 
 import { AuthenticateUSerController } from '@modules/school/useCases/authenticateUser/AuthenticateUserController';
 import { CreateUserController } from '@modules/school/useCases/createUser/CreateUseController';
+import { ListUsersController } from '@modules/school/useCases/listUser/ListUserController';
+
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ensureTeacher } from '../middlewares/ensureTeacher';
 
 const userRouter = Router();
 
 const createUserController = new CreateUserController();
 const authenticateUSerController = new AuthenticateUSerController();
+const listUsersController = new ListUsersController();
 
 userRouter.post('/users', createUserController.handle);
 userRouter.post('/login', authenticateUSerController.handle);
+userRouter.get(
+  '/users',
+  ensureAuthenticated,
+  ensureTeacher,
+  listUsersController.handle
+);
 
 export { userRouter };
