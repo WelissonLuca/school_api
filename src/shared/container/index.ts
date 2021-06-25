c789a859250c8200f4dfc726865ace9b743fa492
WelissonLuca <welissonluca17@gmail.com>
2021-06-25 12:12:50 -0300

    feat: register singleton alternative inject

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,10 @@
 import { container } from 'tsyringe';
 
+import { AlternativeRepository } from '@modules/school/infra/typeorm/repositories/AlternativesRepository';
 import { QuestionRepository } from '@modules/school/infra/typeorm/repositories/QuestionRepository';
 import { SchoolTestRepository } from '@modules/school/infra/typeorm/repositories/SchoolTestRepository';
 import { UserRepository } from '@modules/school/infra/typeorm/repositories/UserRepository';
+import { IAlternativeRepository } from '@modules/school/repositories/IAlternativeRepository';
 import { IQuestionRepository } from '@modules/school/repositories/IQuestionRepository';
 import { ISchoolTestRepository } from '@modules/school/repositories/ISchoolTestRepository';
 import { IUserRepository } from '@modules/school/repositories/IUserRepository';
@@ -15,4 +17,9 @@ container.registerSingleton<ISchoolTestRepository>(
 container.registerSingleton<IQuestionRepository>(
   'QuestionRepository',
   QuestionRepository
+);
+
+container.registerSingleton<IAlternativeRepository>(
+  'AlternativeRepository',
+  AlternativeRepository
 );
