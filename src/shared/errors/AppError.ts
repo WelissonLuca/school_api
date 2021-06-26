67f06bf4638d48e92b2323f8c6b603988118da96
WelissonLuca <welissonluca17@gmail.com>
2021-06-25 22:28:35 -0300

    chore: add jest and supertest

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,11 +1,11 @@
 class AppError {
   public readonly message: string;
 
-  public readonly statusCode: number;
+  public readonly status: number;
 
-  constructor(message: string, statusCode = 400) {
+  constructor(message: string, status = 400) {
     this.message = message;
-    this.statusCode = statusCode;
+    this.status = status;
   }
 }
 
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -18,7 +18,7 @@ app.use(router);
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
+      return response.status(err.status).json({
         message: err.message,
       });
     }
