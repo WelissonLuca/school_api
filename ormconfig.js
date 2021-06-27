a5c99112775182c4606ecf3885fe6ba0f6301f60
WelissonLuca <welissonluca17@gmail.com>
2021-06-27 20:13:01 -0300

    refactor: ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,21 @@
+console.log('process.env.DATABaSE_URL: =>', process.env.DATABASE_URL);
+module.exports = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  migrations: [
+    './src/shared/infra/typeorm/migrations/**.ts' ||
+      './dist/shared/infra/typeorm/migrations/**.js',
+  ],
+  entities: [
+    './src/modules/*/infra/typeorm/entities/**.ts' ||
+      './dist/modules/*/infra/typeorm/entities/**.js',
+  ],
+  cli: {
+    migrationsDir:
+      './src/shared/infra/typeorm/migrations' ||
+      './dist/shared/infra/typeorm/migrations',
+    entities:
+      './src/modules/*/infra/typeorm/entities/**.ts' ||
+      './dist/modules/*/infra/typeorm/entities/**.js',
+  },
+};
