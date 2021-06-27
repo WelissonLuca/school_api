4ab9db63a9637b4429c0fae5e06946b6c0bae5d4
WelissonLuca <welissonluca17@gmail.com>
2021-06-27 20:33:32 -0300

    fix: configure heroku deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,23 +1,14 @@
 console.log('process.env.DATABaSE_URL: =>', process.env.DATABASE_URL);
+
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   ssl: true,
   extra: { ssl: { rejectUnauthorized: false } },
-  migrations: [
-    './src/shared/infra/typeorm/migrations/**.ts' ||
-      './dist/shared/infra/typeorm/migrations/**.js',
-  ],
-  entities: [
-    './src/modules/*/infra/typeorm/entities/**.ts' ||
-      './dist/modules/*/infra/typeorm/entities/**.js',
-  ],
+  migrations: ['./dist/shared/infra/typeorm/migrations/**.js'],
+  entities: ['./dist/modules/*/infra/typeorm/entities/**.js'],
   cli: {
-    migrationsDir:
-      './src/shared/infra/typeorm/migrations' ||
-      './dist/shared/infra/typeorm/migrations',
-    entities:
-      './src/modules/*/infra/typeorm/entities/**.ts' ||
-      './dist/modules/*/infra/typeorm/entities/**.js',
+    migrationsDir: './dist/shared/infra/typeorm/migrations',
+    entities: './dist/modules/*/infra/typeorm/entities',
   },
 };
