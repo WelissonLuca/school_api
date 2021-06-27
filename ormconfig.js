816cb2a5ec1d3cc9c8384551b90dabb2918dc45d
WelissonLuca <welissonluca17@gmail.com>
2021-06-27 20:23:19 -0300

    fix: ormconfig add ssl

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,8 @@ console.log('process.env.DATABaSE_URL: =>', process.env.DATABASE_URL);
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  ssl: true,
+  extra: { ssl: { rejectUnauthorized: false } },
   migrations: [
     './src/shared/infra/typeorm/migrations/**.ts' ||
       './dist/shared/infra/typeorm/migrations/**.js',
